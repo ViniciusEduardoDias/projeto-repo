d7fb84fc6bef1f6103e2266a74519b66789a1203
Vinícius Dias <diasvinicius0401@gmail.com>
2025-08-26 22:28:51 -0300

    feat: adiciona requisição repositorio page

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,13 +1,37 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import api from "../../services/api";
 
 import { Container } from "./styles";
 
-export default function Repositorio() {
+export default function Repositorio({ match }) {
   const { repositorio } = useParams();
+  const [repositorioDatas, setRepositorioDatas] = useState([]);
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function load() {
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${repositorio}`),
+        api.get(`/repos/${repositorio}/issues`),
+        {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        },
+      ]);
+      setRepositorioDatas(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+  }, [repositorio]);
 
   return (
-    <>
-      <Container> {`Repositório: ${repositorio}`} </Container>
-    </>
+    <Container>
+      <h1 style={{ color: "white" }}>{repositorio}</h1>
+    </Container>
   );
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
-`
+  max-width: 900px;
+  margin: auto;
+  padding: 20px;
+  margin-top: 40px;
+  font-family: Arial, Helvetica, sans-serif;
+
+  h1 {
+    font-size: 24px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
