fbe357e1eb5bf6d6493eef852781dc74ca414a40
Vinícius Dias <diasvinicius0401@gmail.com>
2025-09-08 23:14:08 -0300

    feat: corrige requisição e renderiza os dados na tela do repositorio. Style: Estiliza a página de detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import api from "../../services/api";
 
-import { Container } from "./styles";
+import { Container, Loading, Owner, Issues } from "./styles";
 
 export default function Repositorio({ match }) {
   const { repositorio } = useParams();
@@ -15,23 +15,65 @@ export default function Repositorio({ match }) {
     async function load() {
       const [repositorioData, issuesData] = await Promise.all([
         api.get(`/repos/${repositorio}`),
-        api.get(`/repos/${repositorio}/issues`),
-        {
+        api.get(`/repos/${repositorio}/issues`, {
           params: {
             state: "open",
             per_page: 5,
           },
-        },
+        }),
       ]);
+
       setRepositorioDatas(repositorioData.data);
       setIssues(issuesData.data);
       setLoading(false);
     }
+    load();
   }, [repositorio]);
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1 style={{ color: "white" }}>Carregando...</h1>
+      </Loading>
+    );
+  }
 
   return (
     <Container>
-      <h1 style={{ color: "white" }}>{repositorio}</h1>
+      <Owner>
+        <h1>{repositorioDatas.name}</h1>
+        <img
+          src={repositorioDatas.owner.avatar_url}
+          alt={repositorioDatas.owner.login}
+          width={120}
+        />
+      </Owner>
+
+      {issues.length > 0 && (
+        <>
+          <h2 style={{ color: "white", marginTop: 20 }}>Issues abertas</h2>
+          <Issues>
+            {issues.map((issue) => (
+              <li key={issue.id}>
+                <div>
+                  <span>Título</span>
+                  <a href={issue.html_url} target="_blank" rel="noreferrer">
+                    {issue.title}
+                  </a>
+                </div>
+                <div>
+                  <span>Autor</span> <p>Autor: {issue.user.login}</p>
+                </div>
+              </li>
+            ))}
+          </Issues>
+        </>
+      )}
+      {issues && (
+        <h1 style={{ color: "white", marginTop: 20 }}>
+          Nenhuma issue aberta neste repositório
+        </h1>
+      )}
     </Container>
   );
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,14 +1,81 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  background-color: white;
   max-width: 900px;
-  margin: auto;
+  border-radius: 20px;
   padding: 20px;
-  margin-top: 40px;
   font-family: Arial, Helvetica, sans-serif;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 40px auto;
 
   h1 {
     font-size: 24px;
     font-family: Arial, Helvetica, sans-serif;
+  }
+`;
+
+export const Owner = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  box-shadow: 0 0 20px 2px rgba(255, 255, 255, 0.5);
+  padding: 40px;
+
+  img {
+    border-radius: 50%;
+  }
+`;
+
+export const Loading = styled.div`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 0 20px 5px rgba(255, 255, 255, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Issues = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+  padding-top: 30px;
+  border-top: 1px solid #eee;
+  border-radius: 5px;
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    align-items: center;
+    justify-content: space-between;
+    gap: 5px;
+
+    div {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+      span {
+        font-weight: bold;
+        padding: 5px;
+        background: #eee;
+        border-radius: 5px;
+        font-size: 12px;
+      }
+    }
+
+    a {
+      text-decoration: none;
+    }
   }
 `;
